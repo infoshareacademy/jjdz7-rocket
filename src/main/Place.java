a6d291ff3b2128fb707f2d35a11a86e679194947
Adrian Zajączkowski <zajac50@gmail.com>
2019-08-12 20:45:22 +0200

    added Util class and changed Place class

--- src/main/Place.java
+++ src/main/Place.java
@@ -2,4 +2,78 @@ package main;
 
 public class Place {
 
+    private String name;
+    private String street;
+    private String city;
+    private int quantityPlaces;
+    private int quantityGames;
+    private boolean isOpen;
+
+    public Place(String name, String street, String city, int quantityPlaces, int quantityGames, boolean isOpen) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.quantityPlaces = quantityPlaces;
+        this.quantityGames = quantityGames;
+        this.isOpen = isOpen;
+    }
+//gettery
+    public String getName() {
+        return name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getQuantityPlaces() {
+        return quantityPlaces;
+    }
+
+    public int getQuantityGames() {
+        return quantityGames;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+//settery
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setQuantityPlaces(int quantityPlaces) {
+        this.quantityPlaces = quantityPlaces;
+    }
+
+    public void setQuantityGames(int quantityGames) {
+        this.quantityGames = quantityGames;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public String getAdress(){
+        return street + city;
+    }
+
+    public String ckeckGames(){
+        if (quantityGames == 0){
+            return "You need games";
+        }
+    }
+
 }
---
+++ src/main/Util.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.Scanner;
+
+public class Util {
+
+    public static String readUserInput(){
+        return new Scanner(System.in).nextLine();
+    }
+}
