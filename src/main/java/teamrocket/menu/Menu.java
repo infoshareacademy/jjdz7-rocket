43f397aa58df611c4b0638d61757bf1c6a7c7380
sebastian-damps <jumpyjumps@gmail.com>
2019-09-22 16:13:29 +0200

    in progress...

--- src/main/java/teamrocket/menu/Menu.java
+++ src/main/java/teamrocket/menu/Menu.java
@@ -16,8 +16,7 @@ import static teamrocket.model.UserService.list;
 
 public class Menu {
 
-
-    boolean exit;
+     private boolean exit;
 
     public void menuStart() throws IOException {
         printMainMenuInfo();
@@ -28,10 +27,10 @@ public class Menu {
     }
 
     private void printMainMenuInfo() {
-        System.out.println(" ### Menu Aplikacji ### ");
+        System.out.println(" ##### Menu Aplikacji #####  ");
         System.out.println("1: ##### Gry Planszowe  #####");
         System.out.println("2: ##### Wydarzenia     #####");
-        System.out.println("3: ##### Ulbuione       #####");
+        System.out.println("3: ##### Ulubuione      #####");
         System.out.println("0: ##### Wyjście        #####");
     }
 
@@ -41,6 +40,22 @@ public class Menu {
         System.out.println("2: Szukaj gry po jej nazwie ");
         System.out.println("3: Filtruj gry po typie ");
         System.out.println("4: Filtruj gry po liczbie graczy");
+        System.out.println("9: Wróć do głonwego menu");
+        System.out.println("0: Wyjście");
+    }
+
+    private void printEventMenuInfo(){
+        System.out.println("1: Pokaż wszystkie wydarzenia ");
+        System.out.println("2: Filtruj wydarzenia po typie");
+        System.out.println("3: Filtruj wydarzenia po dacie");
+        System.out.println("4: Dodaj nowe wydarzenie ");
+        System.out.println("9: Wróć do głonwego menu");
+        System.out.println("0: Wyjście");
+    }
+
+    private void printFavouritesMenuInfo(){
+        System.out.println("1: Dodaj grę do ulubionych ");
+        System.out.println("2: Pokaż ulubione gry");
         System.out.println("9: Wróć do głonwego menu");
         System.out.println("0: Wyjście");
     }
@@ -59,59 +74,19 @@ public class Menu {
                 break;
 
             case 2:
-                Event.createArrayFromRepo();
-                System.out.println("1: Pokaż wszystkie wydarzenia ");
-                System.out.println("2: Filtruj wydarzenia po typie");
-                System.out.println("3: Filtruj wydarzenia po dacie");
-                System.out.println("4: Dodaj nowe wydarzenie ");
-                System.out.println("9: Wróć do głonwego menu");
-                System.out.println("0: Wyjście");
-
-                switch (Util.readUserInputInteger()) {
-                    case 1:
-                        Event.showAllEvents();
-                        return;
-                    case 2:
-                        Event.filterEventsByType();
-                        break;
-                    case 3:
-                        Event.filterEventByDate();
-                        break;
-                    case 4:
-                        EventService eventService = new EventService();
-                        eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
-                        break;
-                    case 9:
-                        menuStart();
-                    case 0:
-                        exit = true;
-                        break;
-                    default:
-                        printBadChoice();
-                }
+                do {
+                    printEventMenuInfo();
+                    int eventChoice = Util.readUserInputInteger();
+                    decideEvent(eventChoice);
+                } while (!exit);
                 break;
 
             case 3:
-                System.out.println("1: Dodaj grę do ulubionych ");
-                System.out.println("2: Pokaż ulubione gry");
-                System.out.println("9: Wróć do głonwego menu");
-                System.out.println("0: Wyjście");
-
-                switch (Util.readUserInputInteger()) {
-                    case 1:
-                        UserService.addUser(UserService.getUserFromConsole());
-                        break;
-                    case 2:
-                        list(UserService.getUsers());
-                        break;
-                    case 9:
-                        menuStart();
-                    case 0:
-                        exit = true;
-                        break;
-                    default:
-                        printBadChoice();
-                }
+                do {
+                    printFavouritesMenuInfo();
+                    int favouritesChoice = Util.readUserInputInteger();
+                    decideFavourites(favouritesChoice);
+                } while (!exit);
                 break;
             case 0:
                 exit = true;
@@ -146,6 +121,52 @@ public class Menu {
             default:
                 printBadChoice();
         }
+    }
+
+    private void decideEvent(int eventChoice) throws IOException {
+        Event.createArrayFromRepo();
+        switch (Util.readUserInputInteger()) {
+            case 1:
+                Event.showAllEvents();
+                return;
+            case 2:
+                Event.filterEventsByType();
+                break;
+            case 3:
+                Event.filterEventByDate();
+                break;
+            case 4:
+                EventService eventService = new EventService();
+                eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
+                break;
+            case 9:
+                menuStart();
+            case 0:
+                exit = true;
+                break;
+            default:
+                printBadChoice();
+        }
+
+    }
+
+    private void decideFavourites(int favouritesChoice) throws IOException {
+        switch (Util.readUserInputInteger()) {
+            case 1:
+                UserService.addUser(UserService.getUserFromConsole());
+                break;
+            case 2:
+                list(UserService.getUsers());
+                break;
+            case 9:
+                menuStart();
+            case 0:
+                exit = true;
+                break;
+            default:
+                printBadChoice();
+        }
+
     }
 }
 
