7049f62dfb9024bcee183c78d3e449e08e942e1d
sebastian-damps <jumpyjumps@gmail.com>
2019-09-22 09:05:09 +0200

    w miare dziala

--- src/main/java/teamrocket/menu/Menu.java
+++ src/main/java/teamrocket/menu/Menu.java
@@ -21,17 +21,17 @@ public class Menu {
             System.out.println ( "4: ##### EVENT    #####" );
             System.out.println ( "5: ##### Place    #####" );
             System.out.println ( "6: ##### Display  #####" );
-            System.out.println ( "q: ##### Quit     #####" );
-            String choice = Util.readUserInput();
+            System.out.println ( "0: ##### Quit     #####" );
+            int choice = Util.readUserInputInteger();
             do {
-                switch (Util.readUserInputInteger()) {
+                switch (choice) {
                     case 1: {
-                        switch(choice){
-                            case "1":
+                        switch(Util.readUserInputInteger()){
+                            case 1:
                                 System.out.println ( "1: Search for player by Name" );
                             Util.readUserInput();
                                     break;
-                            case "2":
+                            case 2:
                                 System.out.println( "2: Search for player by ID" );
                                 Util.readUserInput();
                                 break;
@@ -62,54 +62,41 @@ public class Menu {
 
                         switch(Util.readUserInputInteger()){
                             case 1: Game.showAllGames();
+                            break;
                             case 2: Game.searchGameByName();
+                            break;
                             case 3: Game.filterByGameType();
+                            break;
                             case 4: Game.filterByNumberOfPlayers();
+                            break;
                         }
 
-                        System.out.println ( "q: Exit" );
-                        while (choice != "q");
-                        break;
                     }
                     case 3:
                         break;
+
                     case 4:
                         Event.createArrayFromRepo();
-                        System.out.println ("1: Show all games " );
-                        System.out.println ("2: Search for a game by name " );
-                        System.out.println("3: Filter Game by Type ");
-                        System.out.println("4: Filter Game by Number of Players ");
+                        System.out.println ("1: Show all Events " );
+                        System.out.println ("2: Filter Event by Type " );
+                        System.out.println("3: Filter Event by Date ");
+                        System.out.println("4: Add new Event ");
 
                         switch(Util.readUserInputInteger()){
                             case 1: Event.showAllEvents();
+                            case 2: Event.filterEventsByType();
+                            case 3: Event.filterEventByDate();
+                            case 4: EventService eventService = new EventService();
+                                eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
                         }
-                        Event.showAllEvents();
-                        Event.filterEventByDate() ;
-                        Event.filterEventsByType();
-                        //dodawanie wydarzenia
-                        EventService eventService = new EventService();
-                        eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
                         break;
                     case 5:
-                        System.out.println ( "Place" );
-                        Util.readUserInput();
-
                         break;
                     case 6:
-                        System.out.println ("### Command Options ### "  );
-                        System.out.println ( "1: # search for user #"   );
-                        System.out.println ( "2: ##### GAME     #####"  );
-                        System.out.println ( "3: ##### ADD Game #####"  );
-                        System.out.println ( "4: ##### EVENT    #####"  );
-                        System.out.println ( "5: ##### Place    #####"  );
-                        System.out.println ( "6: ##### Display  #####"  );
-                        System.out.println ( "q: ##### Quit     #####"  );
-                        Util.readUserInputInteger();
-                        break;
+                        menuStart();
                 }
 
-            }while (choice != "q");
-
+            }while (choice == 0);
                 System.out.println ( "Koniec" );
 
         }
