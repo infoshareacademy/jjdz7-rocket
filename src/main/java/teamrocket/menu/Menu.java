5147fc5ad003b5678cc6d018c8ed8375c9a223bc
sebastian-damps <jumpyjumps@gmail.com>
2019-09-22 10:52:45 +0200

    ...

--- src/main/java/teamrocket/menu/Menu.java
+++ src/main/java/teamrocket/menu/Menu.java
@@ -13,75 +13,96 @@ import java.util.Scanner;
 
 public class Menu {
 
-        public static void menuStart() throws IOException{
-            System.out.println(" ### Command Options ### ");
-            System.out.println("1: ##### GAME     #####");
-            System.out.println("2: ##### EVENT    #####");
-            System.out.println("0: ##### Quit     #####");
-            int choice = Util.readUserInputInteger();
-            switch (choice) {
-                case 1: {
-                    Game.createArrayFromRepo();
-                    System.out.println("1: Show all games ");
-                    System.out.println("2: Search for a game by name ");
-                    System.out.println("3: Filter Game by Type ");
-                    System.out.println("4: Filter Game by Number of Players ");
-                    System.out.println("9: Go back to Main Menu");
-                    System.out.println("0: Exit");
+    public static void menuStart() throws IOException {
+        System.out.println(" ### Command Options ### ");
+        System.out.println("1: ##### GAME       #####");
+        System.out.println("2: ##### EVENT      #####");
+        System.out.println("3: ##### FAVOURITES #####");
+        System.out.println("0: ##### Quit       #####");
+        int choice = Util.readUserInputInteger();
+        switch (choice) {
+            case 1: {
+                Game.createArrayFromRepo();
+                System.out.println("1: Show all games ");
+                System.out.println("2: Search for a game by name ");
+                System.out.println("3: Filter Game by Type ");
+                System.out.println("4: Filter Game by Number of Players ");
+                System.out.println("9: Go back to Main Menu");
+                System.out.println("0: Exit");
 
-                    switch (Util.readUserInputInteger()) {
-                        case 1:
-                            Game.showAllGames();
-                            break;
-                        case 2:
-                            Game.searchGameByName();
-                            break;
-                        case 3:
-                            Game.filterByGameType();
-                            break;
-                        case 4:
-                            Game.filterByNumberOfPlayers();
-                            break;
-                        case 9:
-                            menuStart();
-                        case 0:
-                            break;
-                    }
-                    break;
-
+                switch (Util.readUserInputInteger()) {
+                    case 1:
+                        Game.showAllGames();
+                        break;
+                    case 2:
+                        Game.searchGameByName();
+                        break;
+                    case 3:
+                        Game.filterByGameType();
+                        break;
+                    case 4:
+                        Game.filterByNumberOfPlayers();
+                        break;
+                    case 9:
+                        menuStart();
+                    case 0:
+                        break;
                 }
-                case 2:
-                    Event.createArrayFromRepo();
-                    System.out.println("1: Show all Events ");
-                    System.out.println("2: Filter Event by Type ");
-                    System.out.println("3: Filter Event by Date ");
-                    System.out.println("4: Add new Event ");
-                    System.out.println("9: Go back to Main Menu");
-                    System.out.println("0: Exit");
-
-                    switch (Util.readUserInputInteger()) {
-                        case 1:
-                            Event.showAllEvents();
-                            break;
-                        case 2:
-                            Event.filterEventsByType();
-                            break;
-                        case 3:
-                            Event.filterEventByDate();
-                            break;
-                        case 4:
-                            EventService eventService = new EventService();
-                            eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
-                            break;
-                        case 9:
-                            menuStart();
-                        case 0:
-                            break;
-                    }
-                    break;
-                case 0:
-                    break;
+                break;
             }
+            case 2:
+                Event.createArrayFromRepo();
+                System.out.println("1: Show all Events ");
+                System.out.println("2: Filter Event by Type ");
+                System.out.println("3: Filter Event by Date ");
+                System.out.println("4: Add new Event ");
+                System.out.println("9: Go back to Main Menu");
+                System.out.println("0: Exit");
+
+                switch (Util.readUserInputInteger()) {
+                    case 1:
+                        Event.showAllEvents();
+                        return;
+                    case 2:
+                        Event.filterEventsByType();
+                        break;
+                    case 3:
+                        Event.filterEventByDate();
+                        break;
+                    case 4:
+                        EventService eventService = new EventService();
+                        eventService.addEvent(eventService.getEventFromConsole(), eventService.getEventID());
+                        break;
+                    case 9:
+                        menuStart();
+                    case 0:
+                        break;
+                }
+                break;
+
+            case 3:
+                System.out.println("1: Add Game to favourites ");
+                System.out.println("2: Show favourite games");
+                System.out.println("9: Go back to Main Menu");
+                System.out.println("0: Exit");
+
+                switch (Util.readUserInputInteger()) {
+                    case 1:
+                        // method to add games to favourites
+                        break;
+                    case 2:
+                        // method to show favourite games
+                        break;
+                    case 9:
+                        menuStart();
+                    case 0:
+                        break;
+                }
+                break;
+            case 0:
+                break;
         }
+    }
+
 }
 
