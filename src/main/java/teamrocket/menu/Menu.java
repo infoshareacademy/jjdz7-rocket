34b4df4a37398cd1fe7c1e7027698dfaa8afb6dc
sebastian-damps <jumpyjumps@gmail.com>
2019-09-22 09:28:32 +0200

    Deleted exception from method signature. it is handled in methods

--- src/main/java/teamrocket/menu/Menu.java
+++ src/main/java/teamrocket/menu/Menu.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class Menu {
 
-        public static void menuStart() throws IOException {
+        public static void menuStart(){
             System.out.println(" ### Command Options ### ");
             System.out.println("1: ##### GAME     #####");
             System.out.println("2: ##### EVENT    #####");
