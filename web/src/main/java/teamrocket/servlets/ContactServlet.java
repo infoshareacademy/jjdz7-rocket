2952f8d8814610ca473ea263ebdb4be89588aa2b
Adrian Zajączkowski <zajac50@gmail.com>
2019-10-16 21:45:29 +0200

    Fixed the error

--- web/src/main/java/teamrocket/servlets/ContactServlet.java
+++ web/src/main/java/teamrocket/servlets/ContactServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/kontakt")
+@WebServlet("/contact")
 public class ContactServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
