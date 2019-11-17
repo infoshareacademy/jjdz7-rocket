cb74d19236ee42cc698d9645a77bb163641cf781
Adrian Zajączkowski <zajac50@gmail.com>
2019-11-17 11:47:52 +0100

    Main servlet added

---
+++ web/src/main/java/teamrocket/servlets/MainServlet.java
@@ -0,0 +1,37 @@
+package teamrocket.servlets;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import teamrocket.freemarker.TemplateProvider;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/main")
+public class MainServlet extends HttpServlet {
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+        Map<String,Object> model = new HashMap<>();
+        Template template = templateProvider.getTemplate(getServletContext(),"StronaGł.Rocket.ftlh");
+
+        try {
+            template.process(model, writer);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
