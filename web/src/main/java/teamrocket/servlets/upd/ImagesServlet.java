93b51f54b315895943bdac4281e13d753d3823ad
jakub <kuba.marcinkowski98@wp.pl>
2020-01-10 16:05:42 +0100

    Merge branch 'develop' of https://github.com/infoshareacademy/jjdz7-rocket into JM
     Conflicts:
    	web/src/main/webapp/html-style/wydazenia.html
    
    Signed-off-by: jakub <kuba.marcinkowski98@wp.pl>

--- web/src/main/java/teamrocket/servlets/upd/ImagesServlet.java
+++ web/src/main/java/teamrocket/servlets/upd/ImagesServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/images/*")
+@WebServlet("/images")
 public class ImagesServlet extends HttpServlet {
 
   @Inject
